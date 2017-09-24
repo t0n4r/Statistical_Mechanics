c60974ddd449f91f47e93ba57254af13992df975
Murshed Al-Amin <tonar.tron@gmail.com>
2017-09-24 20:28:18 +0600

    Update direct-pi.cpp

--- direct-pi.cpp
+++ direct-pi.cpp
@@ -6,20 +6,22 @@ int main()
 {
     srand(time(NULL));
 
-    int n_trials = 400, n_hits = 0;
-   
+    int n_trials, n_hits = 0;
+    
+    cin >> n_trials;
+    
     for (int i = 1; i < n_trials; i++)
     {
-        float x = rand() % 3 + (-1);
-        float y = rand() % 3 + (-1);
+        float x = 2.0 * drand48() + (-1);
+        float y = 2.0 * drand48() + (-1);
 
-        if (pow(x, 2) + pow(y, 2) < 1)
+        if (pow(x, 2) + pow(y, 2) <= 1)
         {
             n_hits = n_hits + 1;
         }
     }
     
-    cout << "Pi is approx. " << (4.0 * n_hits) / 400 << endl;
+    cout << "Pi is approx. " << (4.0 * n_hits) / n_trials << endl;
 
     return 0;
 }
